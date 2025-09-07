b3aa785cc1a7dcaa7417074aea7c3d5ccc2e55e6
paulo4ciz <pau.troncoso@duocuc.cl>
2025-09-06 22:15:29 -0400

    segundo commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,16 @@
 function Body() {
   return (
     <main style={{ minHeight: "70vh", padding: "2rem", textAlign: "center" }}>
-      <h2>Bienvenido a mi primera página en React</h2>
-      <p>Este es el cuerpo (body) donde irá el contenido principal.</p>
+      <h2>Bievenidos a nuestra pagina "Donde esta la mano"</h2>
+      <p>En esta primera fase logramos:
+
+Subir el proyecto React (Capstone) a un repositorio en GitHub.
+Configurar un flujo de Integración Continua (CI/CD) con Azure Static Web Apps, de manera que cada cambio en la rama principal (main) dispara automáticamente:
+La construcción del proyecto (npm run build).
+El despliegue en Azure con HTTPS y CDN habilitados.
+Implementar un archivo de configuración (staticwebapp.config.json) que asegura la correcta navegación entre páginas de la aplicación (rutas SPA).
+
+✅ Con esto, el proyecto ya cuenta con una URL pública accesible desde cualquier navegador, y un flujo automatizado de publicación cada vez que se suben cambios al repositorio.</p>
     </main>
   );
 }
